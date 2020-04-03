650130cd5acc33001aa54439542a8ee8f7c34bd5
takhtoukh <stakhto@gmail.com>
2020-04-03 11:02:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("form.html")
+    return render_template("index.html")
 
 
 @app.route('/resultat', methods=['POST'])
